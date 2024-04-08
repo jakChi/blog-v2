eddcd2a081ad2bb35318dfbea4bdde58968d93ac
Your Name <you@example.com>
2024-04-08 19:37:27 +0400

    minor changes in nav

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import { Inter } from "next/font/google";
 import 'bootstrap/dist/css/bootstrap.css';
 import "./globals.css";
-import Navbar from "../components/Navbar";
+import Navbar from "@/components/Navbar.1";
 import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
--- tailwind.config.js
+++ tailwind.config.js
@@ -14,5 +14,6 @@ module.exports = {
       },
     },
   },
+  prefix: "tw-",
   plugins: [],
 };
