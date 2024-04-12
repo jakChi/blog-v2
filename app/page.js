700a68e1b8c6683294dc26f42da64f2dec1a3478
jakChi <kobachincharauli8@gmail.com>
2024-04-12 20:03:16 +0400

    page modified

--- app/page.js
+++ app/page.js
@@ -1,28 +1,21 @@
-"use client"
-
 import BlogCard from "@/components/Blog-card";
-import { useState, useEffect } from "react";
 export const BLOG_API =
   "https://660e6f836ddfa2943b36c78e.mockapi.io/api/v2/blogs";
 export const USER_API =
   "https://660e6f836ddfa2943b36c78e.mockapi.io/api/v2/users";
 
-export default function Home() {
-  const [blogs, setBlogs] = useState([]);
+const getBlogs = async () => {
+  try {
+    const response = await fetch(BLOG_API);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching blogs:", error);
+  }
+};
 
-  useEffect(() => {
-    const getBlogs = async () => {
-      try {
-        const response = await fetch(BLOG_API);
-        const data = await response.json();
-        setBlogs(data);
-      } catch (error) {
-        console.error("Error fetching blogs:", error);
-      }
-    };
-
-    getBlogs();
-  }, []); // Empty dependency array to ensure useEffect runs only once
+export default async function Home() {
+  const blogs = await getBlogs();
 
   return (
     <section>
